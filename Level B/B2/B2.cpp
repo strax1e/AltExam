9c81a546570200bc76045d61760613818aeca93a
strax1e <strax1e@yandex.ru>
2020-05-12 02:05:06 +0300

    newB2andRenameOldB2toB3

--- Level B/B2/B2.cpp
+++ Level B/B2/B2.cpp
@@ -1,58 +1,22 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 
-/* Ссылка на задачу:
- * https://codeforces.com/problemset/problem/1034/A
- */
-
-#include <math.h>
-#include <stdio.h>
-
-#define MN 300000
-#define MX 15000000
-int input[MN + 5], least_prime_div[MX + 5], primes[MX + 5], pnum = 0, s[MX + 5];
-
-int gcd( int x, int y )
+int main()
 {
-  return y ? gcd( y, x % y ) : x;
-}
-
-int main( void )
-{
-  int num, allgcd = 0, curdiv, ans = 0;
-  // Поиск простых чисел и чисел, которые на них делятся
-  for (int i = 2; i <= MX; ++i)
-  {
-    if (!least_prime_div[i])
-      least_prime_div[i] = primes[++pnum] = i;
-    for (int j = 1; i * primes[j] <= MX; ++j)
+    long long a(0), b(0), k(0);
+    std::cin >> a >> b;
+    while (a && b)
     {
-      least_prime_div[i * primes[j]] = primes[j];
-      if (i % primes[j] == 0)
-        break;
+        if (a < b)
+        {
+            k += b / a;
+            b %= a;
+        }
+        else
+        {
+            k += a / b;
+            a %= b;
+        }
     }
-  }
-  // Ввод и нахождение НОД
-  scanf( "%d", &num );
-  for (int i = 1; i <= num; ++i)
-  {
-    scanf( "%d", &input[i] );
-    allgcd = gcd( allgcd, input[i] );
-  }
-  /* Подсчет сколько раз каждое простое число
-   * оказалось минимальным делителем для
-   * чисел из введенного набора, сокращенных на НОД
-   */
-  for (int i = 1; i <= num; ++i)
-    for (int j = input[i] / allgcd; j > 1;)
-    {
-      curdiv = least_prime_div[j];
-      s[curdiv]++;
-      do
-        j /= least_prime_div[j];
-      while (least_prime_div[j] == curdiv);
-    }
-  for (int i = 1; i <= MX; ++i)
-    ans = fmax( ans, s[i] );
-  printf( "%d", ans ? num - ans : -1 );
-  return 0;
+    std::cout << k;
+    return 0;
 }
