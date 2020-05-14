0be6372be2c03010b7b48f8060ae7ecce27dca81
V7074 <waiddew@gmail.com>
2020-05-14 22:12:14 +0300

    B6

---
+++ Level B/B6/B6.cpp
@@ -0,0 +1,44 @@
+// https://codeforces.com/problemset/problem/798/C
+
+#include <iostream>
+
+int gcd( int a, int b )
+{
+  while (b)
+  {
+    int t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+int main()
+{
+  int n, elem, ans = 0, currGCD = 0, countOdd = 0;
+  std::cin >> n;
+
+  for (int i = 0; i < n; ++i)
+  {
+    std::cin >> elem;
+    currGCD = gcd( elem, currGCD );
+
+    if (elem % 2)
+      ++countOdd;
+    else
+    {
+      ans += countOdd / 2 + countOdd % 2 * 2;
+      countOdd = 0;
+    }
+  }
+
+  ans += countOdd / 2 + countOdd % 2 * 2;
+
+  std::cout << "YES\n";
+  if (currGCD == 1)
+    std::cout << ans;
+  else
+    std::cout << 0;
+
+  return 0;
+}
